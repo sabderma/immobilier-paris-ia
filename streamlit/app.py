85a7036bcef345b1192dcb0bc7dd0a5d80b46beb
Malek silarbi <maleksilarbi@gmail.com>
2026-06-25 23:34:00 +0200

    Mise à jour du projet sans les CSV

--- streamlit/app.py
+++ streamlit/app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 
 st.set_page_config(
-    page_title="Immobilier Paris",
+    page_title="Immobilier paris",
     page_icon="🏙️",
     layout="wide",
     initial_sidebar_state="collapsed",
